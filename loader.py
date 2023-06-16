05723c243c799dce98965b9ddad2d576e469c2b5
Jms Dnns <jdennis@gmail.com>
2023-06-16 12:57:41 -0400

    Modified PunksDataset to be based on a single data source, in preparation for using random samples

--- loader.py
+++ loader.py
@@ -6,9 +6,32 @@ import matplotlib.image as mpimg
 from torch.utils.data import Dataset
 
 
+PUNK_LABELS = 'data/punks.json'
 TRAIN_LABELS = 'data/train.json'
 TEST_LABELS = 'data/test.json'
 IMG_DIR = 'data/images'
+
+
+ALL_FILTERS = [
+    '3DGlasses', 'alien', 'ape', 'bandana', 'beanie', 'bigBeard', 'bigShades',
+    'blackLipstick', 'blondeBob', 'blondeShort', 'blueEyeShadow', 'buckTeeth',
+    'cap', 'capForward', 'chinstrap', 'choker', 'cigarette', 'classicShades',
+    'clownEyesBlue', 'clownEyesGreen', 'clownHairGreen', 'clownNose',
+    'cowboyHat', 'crazyHair', 'darkHair', 'doRag', 'earring', 'eyeMask',
+    'eyePatch', 'fedora', 'female', 'frontBeard', 'frontBeardDark', 'frown',
+    'frumpyHair', 'goat', 'goldChain', 'greenEyeShadow', 'halfShaved',
+    'handlebars', 'headband', 'hoodie', 'hornedRimGlasses', 'hotLipstick',
+    'knittedCap', 'luxuriousBeard', 'male', 'medicalMask', 'messyHair',
+    'mohawk', 'mohawkDark', 'mohawkThin', 'mole', 'mustache', 'muttonchops',
+    'nerdGlasses', 'normalBeard', 'normalBeardBlack', 'orangeSide',
+    'peakSpike', 'pigtails', 'pilotHelmet', 'pinkWithHat', 'pipe', 'policeCap',
+    'purpleEyeShadow', 'purpleHair', 'purpleLipstick', 'redMohawk',
+    'regularShades', 'rosyCheeks', 'shadowBeard', 'shavedHead', 'silverChain',
+    'smallShades', 'smile', 'spots', 'straightHair', 'straightHairBlonde',
+    'straightHairDark', 'stringyHair', 'tassleHat', 'tiara', 'topHat',
+    'vampireHair', 'vape', 'vr', 'weldingGoggles', 'wildBlonde', 'wildHair',
+    'wildWhiteHair', 'zombie'
+]
 
 
 def load_image(id, img_dir=IMG_DIR):
@@ -23,40 +46,39 @@ def make_punks_df(labels_file):
     with open(labels_file) as f:
         punks = json.loads(f.read())
     df = pd.DataFrame.from_dict(punks, orient='index')
+
     df['img'] = df.apply(lambda row: [load_image(int(row.name))], axis=1)
+
     return df
 
 
-def gimme_one(df, filter, row_num):
-    row = df.iloc[row_num]
-    data = np.array(row['img'][0])
-    labels = np.array(row[filter])
-    return data, labels
+def df_split(df, test_size=0):
+    df_size = len(df)
+    df_indices = list(range(df_size))
+    np.random.shuffle(df_indices)
 
+    a_idx = df_indices[test_size:]
+    b_idx = df_indices[:test_size]
 
-def any_to_one(i):
-    x = True
-    if i['any']:
-        x = False
-    return int(x)
+    return a_idx, b_idx
 
 
 class PunksDataset(Dataset):
-    def __init__(self, filter, img_dir=IMG_DIR, train=False):
+    def __init__(
+            self, filter, test_size=0, img_dir=IMG_DIR, labels_path=PUNK_LABELS
+    ):
         self.filter = filter
         self.img_dir = img_dir
+        self.labels_path = labels_path
 
-        self.labels_path = TRAIN_LABELS if train else TEST_LABELS
         punks_df = make_punks_df(self.labels_path)
+        self.train_idx, self.test_idx = df_split(punks_df, test_size)
+
         self.X = np.array([row[0] for row in punks_df['img'].to_numpy()])
         self.Y = punks_df[filter].to_numpy()
-        # punks_df['any'] = punks_df[filter].apply(np.any, axis=1)
-        # punks_df['none'] = punks_df.apply(lambda x: any_to_one(x), axis=1)
-        # self.Y = punks_df[filter + ['none']].to_numpy()
 
     def __len__(self):
         return len(self.Y)
 
     def __getitem__(self, idx):
         return self.X[idx], self.Y[idx]
-        # return gimme_one(self.punks_df, self.filter, idx)
